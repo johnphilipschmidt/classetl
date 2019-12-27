fc77c820bd733ea9888f1691fae5d81a4f312ff5
Phil <johnphilipschmidt@gmail.com>
2019-12-27 10:53:55 -0500

    Changed map to assoicate the pattern with the attribute substring mapping

--- classificationMapper.py
+++ classificationMapper.py
@@ -11,27 +11,48 @@ import os, os.path, string,sys, re
 
 
 def getPattern(source):
-        pattern=['re.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)', #CAN S//OC//NF//LES
-                 're.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/FISA",source)', #CAN S//OC//NF//FISA
-                 're.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]",source)', #CAN S//OC
-                 're.search("^//[A-Z][A-Z][A-Z] [UCS]/[A-Z][A-Z]",source)', #CAN S/OC
-                 're.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]",source)', #CAN S//OC//NF
-                 're.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]//LES",source)', #CAN S//OC//NF//LES
-                 're.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)' #CAN S//OC//NF/LES
-                ]
+        #pattern=[['re.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)','A'],#CAN S//OC//NF//LES
+         #        ['re.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/FISA",source)','B'], #CAN S//OC//NF//FISA
+          #       ['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]",source)','C'], #CAN S//OC
+          #       ['re.search("^//[A-Z][A-Z][A-Z] [UCS]/[A-Z][A-Z]",source)','D'],#CAN S/OC
+           #      ['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]",source)', 'E'],#CAN S//OC//NF
+            #     ['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]//LES",source)','F'], #CAN S//OC//NF//LES
+            #     ['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)','G'] #CAN S//OC//NF/LES
+            #    ]
+        #print(source)
+
+        data = {
+            'item1': ['pattern1','AA','BB'],
+            'item2': ['pattern2','AA','BB']
+        }
+
+        pattern={
+                #'pattern0': ['//CAN S//NC','a Pattern',{'classification':'0:3','nocitem':'4:3'}],
+                'pattern0':['re.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)',{'classifications':'0:3','dissemintationControls':'4:5'}],#CAN S//OC//NF//LES
+                'pattern1':['re.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/FISA",source)',{'classifications':'0:3','dissemintationControls':'4:5'}], #CAN S//OC//NF//FISA
+                'pattern2':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]",source)',{'classifications':'0:3','dissemintationControls':'4:5'}], #CAN S//OC
+                'pattern3':['re.search("^//[A-Z][A-Z][A-Z] [UCS]/[A-Z][A-Z]",source)',{'classifications':'0:3','dissemintationControls':'4:5'}],#CAN S/OC
+                'pattern4':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]",source)',{'classifications':'0:3','dissemintationControls':'4:5'}],#CAN S//OC//NF
+                'pattern5':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]//LES",source)',{'classifications':'0:3','dissemintationControls':'4:5'}], #CAN S//OC//NF//LES
+                'pattern6':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)',{'classifications':'0:3','dissemintationControls':'4:5'}]#CAN S//OC//NF/LES
+                 }
         for i in range(0,len(pattern)):
-            result=eval(pattern[i])
+#for i in range(len(data)):
+#    x=eval(data['pattern'+str(i)][0])
+#    if ( x != None):
+#        print ('pattern'+str(i)+" "+ str(x.start()))
 
-            if (result != None):
-                #print("pattern"+str(i)+":"+source)
-                return result
-
+            x=eval(pattern['pattern'+str(i)][0])
+            if (x != None):
+                return  x
 def main():
         with open("c.dat") as file:
             for line in file:
                 if(len(line) > 1):
                     if(getPattern(line) != None):
-                        print (getPattern(line).start())
+                        pattern=getPattern(line)
+                        print (pattern[0])
+
 
 #execute program
 main()
