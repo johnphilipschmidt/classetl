60ac6e8da88d4365d16bbcf69b1aabe79089ed09
Phil <johnphilipschmidt@gmail.com>
2019-12-27 15:41:37 -0500

    Added Fd930- and fields to data file, added mapping fopr parsing substr dynamically by changing the key: list ofintegers [2,4] for processing of substr[2:4]

--- classificationMapper.py
+++ classificationMapper.py
@@ -2,56 +2,70 @@ import os, os.path, string,sys, re
 
 
 #classifications: U C S
-#dissemintationControls: RS, OC OC-USGOV, IMC, NF, PR,REL,RELIDO, DSEN, FISA
+#'ism:disseminationControls: RS, OC OC-USGOV, IMC, NF, PR,REL,RELIDO, DSEN, FISA
 #NonIcMarkings XD, ND, LES, LES-NF, SSI
 #TypeOfExemptedSource XD, ND, LES, LES-NF, SSI
 #FGIsourceOpen US ONLY, JOINT, Mixed US/FGI Protected, FGI Protected, Mixed US/FGI Opn, FGI Open
 #ism:classification="S" ism:ownerProducer="DNK", ism:FGIsourceOpen="DNK" ism:nonICMarkings="LES" ism:typeOfExmptedSource="FGI Open"
 #ism:disseminationControls="FISA" ism:nonICMarkings="LES"
 
-
-def getPattern(source):
-        #pattern=[['re.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)','A'],#CAN S//OC//NF//LES
-         #        ['re.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/FISA",source)','B'], #CAN S//OC//NF//FISA
-          #       ['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]",source)','C'], #CAN S//OC
-          #       ['re.search("^//[A-Z][A-Z][A-Z] [UCS]/[A-Z][A-Z]",source)','D'],#CAN S/OC
-           #      ['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]",source)', 'E'],#CAN S//OC//NF
-            #     ['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]//LES",source)','F'], #CAN S//OC//NF//LES
-            #     ['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)','G'] #CAN S//OC//NF/LES
-            #    ]
-        #print(source)
-
-        data = {
-            'item1': ['pattern1','AA','BB'],
-            'item2': ['pattern2','AA','BB']
-        }
-
-        pattern={
-                #'pattern0': ['//CAN S//NC','a Pattern',{'classification':'0:3','nocitem':'4:3'}],
-                'pattern0':['re.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)',{'classifications':'0:3','dissemintationControls':'4:5'}],#CAN S//OC//NF//LES
-                'pattern1':['re.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/FISA",source)',{'classifications':'0:3','dissemintationControls':'4:5'}], #CAN S//OC//NF//FISA
-                'pattern2':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]",source)',{'classifications':'0:3','dissemintationControls':'4:5'}], #CAN S//OC
-                'pattern3':['re.search("^//[A-Z][A-Z][A-Z] [UCS]/[A-Z][A-Z]",source)',{'classifications':'0:3','dissemintationControls':'4:5'}],#CAN S/OC
-                'pattern4':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]",source)',{'classifications':'0:3','dissemintationControls':'4:5'}],#CAN S//OC//NF
-                'pattern5':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]//LES",source)',{'classifications':'0:3','dissemintationControls':'4:5'}], #CAN S//OC//NF//LES
-                'pattern6':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)',{'classifications':'0:3','dissemintationControls':'4:5'}]#CAN S//OC//NF/LES
-                 }
-        for i in range(0,len(pattern)):
-#for i in range(len(data)):
-#    x=eval(data['pattern'+str(i)][0])
-#    if ( x != None):
-#        print ('pattern'+str(i)+" "+ str(x.start()))
-
+#match the pattern and return the parsing map
+def getPattern(source,pattern):
+    for i in range(0,len(pattern)):
             x=eval(pattern['pattern'+str(i)][0])
             if (x != None):
-                return  x
+                return  'pattern'+str(i)
+
+
+def parsePattern(pattern,patternMatch,source):
+    #define the output map
+    outPutMap={}
+    # get the element dict for the pattern that matched
+    attributeMap=eval(str(pattern[str(patternMatch)][1]))
+
+    #Iterate throught  thekeys to get the substring definition
+    for elementName in attributeMap.keys():
+        #attribute map is a list of two values(start and end)
+        attKey=(attributeMap[elementName])
+        #Get the subtring value fromthe matched pattern in source
+        matchedPattern=source[attKey[0]:attKey[1]]
+        outPutMap[elementName]=matchedPattern
+
+    return outPutMap
+
+
+
+
 def main():
-        with open("c.dat") as file:
-            for line in file:
-                if(len(line) > 1):
-                    if(getPattern(line) != None):
-                        pattern=getPattern(line)
-                        print (pattern[0])
+    #The Masterm map
+    # mapkey, regex pattern to search for dynamic eval execution, attribute map of substring parse range
+    patternMap={
+        'pattern0':['re.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)',{'ism:classifications':[4,5],'ism:disseminationControls':[7,9]},'CAN S//OC '], #CAN S//OC//NF//LES
+        'pattern1':['re.search("^[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/FISA",source)',{'ism:classifications':[4,5],'ism:disseminationControls':[8,9]},'1'], #CAN S//OC//NF//FISA
+        'pattern2':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]",source)',{'ism:classifications':[6,7],'ism:disseminationControls':[9,11]},'2'], #CAN S//OC
+        'pattern3':['re.search("^//[A-Z][A-Z][A-Z] [UCS]/[A-Z][A-Z]",source)',{'ism:classifications':[6,7],'ism:disseminationControls':[7,9]},'3'],#CAN S/OC
+        'pattern4':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]",source)',{'ism:classifications':[6,7],'ism:disseminationControls':[4,5]},'4'],#CAN S//OC//NF
+        'pattern5':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]//LES",source)',{'ism:classifications':[6,7],'ism:disseminationControls':[4,5]},'5'], #CAN S//OC//NF//LES
+        'pattern6':['re.search("^//[A-Z][A-Z][A-Z] [UCS]//[A-Z][A-Z]//[A-Z][A-Z]/LES",source)',{'ism:classifications':[6,7],'ism:disseminationControls':[4,5]},'6']#CAN S//OC//NF/LES
+    }
+    with open("c.dat") as file:
+        result={}
+
+        for line in file:
+            if(len(line) > 1):
+                #split the line by comma 0=ClassString, 2 fd930id,3=columnName
+                lineElements=line.split(',')
+
+
+                #get the pattern key
+                patternMatch=getPattern(lineElements[0],patternMap)
+                if ((patternMatch) != None):
+                    #print ("pattern key is:"+patternMatch)
+                    result[lineElements[1]+') ('+lineElements[2]+') ='+lineElements[2]]=parsePattern(patternMap,patternMatch,lineElements[0])
+
+        for key in result:
+                print ((key.lstrip('\''),(result[key])))
+
 
 
 #execute program
